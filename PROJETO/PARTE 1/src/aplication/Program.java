fe8a3e3cde8f5633e5bfe2f427bb8914ea62b894
Patricknunes24 <patrickwandersonnunes@gmail.com>
2023-11-12 19:21:43 -0300

    att

--- PROJETO/PARTE 1/src/aplication/Program.java
+++ PROJETO/PARTE 1/src/aplication/Program.java
@@ -1,10 +1,16 @@
 package aplication;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         Department db = new Department("patrick",5);
+        Seller seller = new Seller(1,"patrick","patrick@gmail.com",new Date(),100,new Department("patrick",2));
 
+        System.out.println(seller.toString());
+        System.out.println(db.toString());
     }
 }
